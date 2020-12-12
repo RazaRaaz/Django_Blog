582926b6b29ca1d0e9b75934e71689ebc87e818b
raza <raza.0000.arj@gmail.com>
2020-12-12 19:42:06 +0500

    Created Models and simple template setup

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Blog, Comment
 
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+       list_display = ["owner", "title", "body", "created_time", "get_likes"]
+
+admin.site.register(Blog, BlogAdmin) 
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+       owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog")
+       title = models.CharField(max_length=1000)
+       blog_img = models.ImageField(upload_to = 'pic_folder/')
+       body = models.TextField()
+       like = models.ManyToManyField(User, related_name='blog_posts', blank=True)
+       created_time = models.DateTimeField(auto_now_add=True, null=True)
+
+       def get_likes(self):
+              return "\n".join([str(p) for p in self.like.all()])
+
+
+class Comment(models.Model):
+       owner = models.ForeignKey(User, on_delete=models.CASCADE)
+       blog = models.ForeignKey(
+              Blog, 
+              on_delete=models.CASCADE, 
+              related_name="comnents"
+       )
+       body = models.TextField()
+       like = models.ManyToManyField(User, related_name="blog_comments", blank=True)
+       created_time = models.DateTimeField(auto_now_add=True, null=True)
